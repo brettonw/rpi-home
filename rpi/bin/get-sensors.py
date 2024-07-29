701c7a7ef33ea4541974a5a9b8d1f34050e95f9e
Bretton Wade <bretton.wade@gmail.com>
2024-07-29 15:40:04 -0400

    main - missed one

--- rpi/bin/get-sensors.py
+++ rpi/bin/get-sensors.py
@@ -1,11 +1,11 @@
 #! /usr/local/rpi_home/python3/bin/python3
 
-from rpi_home import RpiSensorDevice
+from rpi_home import RpiHomeDevice
 import json
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
-    sensor_device = RpiSensorDevice()
-    print(json.dumps(sensor_device.report()))
+    device = RpiHomeDevice()
+    print(json.dumps(device.report()))
