9df903a80a26be2d28f793e43de8be978ad04271
Bretton Wade <bretton.wade@gmail.com>
2024-07-29 13:05:25 -0400

    main - trying another way...

--- pip/rpi_home/rpi_home/version.py
+++ pip/rpi_home/rpi_home/version.py
@@ -1,14 +1,5 @@
-import toml
 import os
+import importlib.metadata as metadata
 
-from .const import VERSION
-
-
-def _get_version():
-    pyproject_path = os.path.join(os.path.dirname(__file__), "..", "pyproject.toml")
-    with open(pyproject_path, "r") as pyproject_file:
-        pyproject_data = toml.load(pyproject_file)
-        return pyproject_data["project"][VERSION]
-
-
-RPI_HOME_VERSION = _get_version()
+package_name = os.path.basename(os.path.dirname(__file__))
+RPI_HOME_VERSION = metadata.version(package_name)
