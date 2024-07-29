55fb90a25627fb60afef48bf2fff3dadc64b254d
Bretton Wade <bretton.wade@gmail.com>
2024-07-29 13:06:17 -0400

    main - across all the modules

--- rpi/drivers/host/rpi_home_host/version.py
+++ rpi/drivers/host/rpi_home_host/version.py
@@ -1,13 +1,5 @@
-import toml
 import os
+import importlib.metadata as metadata
 
-from rpi_home import VERSION
-
-
-def _get_version():
-    pyproject_path = os.path.join(os.path.dirname(__file__), "..", "pyproject.toml")
-    with open(pyproject_path, "r") as pyproject_file:
-        pyproject_data = toml.load(pyproject_file)
-        return pyproject_data["project"][VERSION]
-
-DRIVER_VERSION = _get_version()
+package_name = os.path.basename(os.path.dirname(__file__))
+DRIVER_VERSION = metadata.version(package_name)
