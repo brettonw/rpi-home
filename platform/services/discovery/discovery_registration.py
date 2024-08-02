5807f99676749b211df8888b9cc67c2cd6a7ed9f
Bretton Wade <bretton.wade@gmail.com>
2024-08-02 13:00:56 -0400

    main - removing extraneous console handler

--- platform/services/discovery/discovery_registration.py
+++ platform/services/discovery/discovery_registration.py
@@ -17,13 +17,6 @@ logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(leve
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-# Add a console handler
-console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
-
 # logging.getLogger(_ZEROCONF).setLevel(logging.DEBUG)
 
 # set up the zeroconf
