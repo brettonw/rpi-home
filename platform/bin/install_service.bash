457d181c8933523b7048a6605ca49b7359556ca8
Bretton Wade <bretton.wade@gmail.com>
2024-08-03 16:54:25 -0400

    main - a little tweak

--- platform/bin/install_service.bash
+++ platform/bin/install_service.bash
@@ -12,22 +12,22 @@ systemd_dir="/lib/systemd/system";
 
 # stop the service if it is already installed
 if [ -f "$systemd_dir/$service_name" ]; then
-  echo "stopping existing \"$service_name\"...";
+  echo "  stopping existing \"$service_name\"...";
   sudo systemctl stop "$service_name";
 fi
 
 # rewrite the service file and copy it to the lib directory
-echo "installing \"$service_name\"...";
+echo "  installing \"$service_name\"...";
 perl -pe "s/\Q{user}\E/$USER/g" "$source_dir/$service_name" > "$source_dir/$service_name.tmp";
 sudo cp "$source_dir/$service_name.tmp" "$systemd_dir/$service_name"
 rm -f "$source_dir/$service_name.tmp";
 
 # enable the service
-echo "enabling \"$service_name\"...";
+echo "  enabling \"$service_name\"...";
 sudo systemctl enable "$service_name";
 
 # start the service
-echo "starting \"$service_name\"...";
+echo "  starting \"$service_name\"...";
 sudo systemctl start "$service_name";
 
-echo "finished installing \"$service_name\"";
+echo "  finished installing \"$service_name\"";
